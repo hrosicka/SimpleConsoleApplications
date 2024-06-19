f13ff22def629dc336f364038f1e563e1792c750
Hanka <hrosicka@post.cz>
2024-06-19 17:24:50 +0200

    File Statitics - functions for get filename, open file and read numbers

--- Basics05/FileStatistics.cpp
+++ Basics05/FileStatistics.cpp
@@ -2,69 +2,91 @@
 
 #include <iostream>     // Program components must include the iostream header file
 #include <fstream>      // Analogy for working with files, for I/O operations to/from files
-#include <cstdlib>      // Support for the exit() object
+#include <vector>
+#include <stdexcept>
 
 using namespace std;    // The program must include the namespace
 
-const int SIZE = 60;
+// Function to get the filename from the user
+string getFilename()
+{
+    string filename;
+    cout << "Enter the name of the file you want to read from: ";
+    getline(cin, filename);
+    return filename;
+}
+
+// Function to open the file and handle errors
+ifstream openFile(const string& filename)
+{
+    ifstream infile(filename);
+    if (!infile.is_open())
+    {
+        throw runtime_error("Failed to open file: " + filename);
+    }
+    return infile;
+}
+
+// Function to read numbers from the file and store them in a vector
+vector<double> readNumbers(ifstream& infile)
+{
+    vector<double> numbers;
+    double number;
+    while (infile >> number)
+    {
+        numbers.push_back(number);
+    }
+    return numbers;
+}
 
 int main()
 
 {
-    char filename[SIZE];
-
-    // Object definition
-    ifstream infile;                // ifstream object
-    ofstream outfile;               // ofstream object
-
-    // Assign objects to files
-    cout << "Enter the name of the file you want to read from" << endl;
-    cin.getline(filename, SIZE);
-    infile.open(filename);          // Read from a file using infile1
-
-    if (!infile.is_open())          // Check if the file is open
+    try 
     {
-        cout << "Failed to open file" << endl;
-        cout << "Program will be terminated." << endl;
-        exit(EXIT_FAILURE);         // Terminate the program
-    }
+        string filename = getFilename();
+        ifstream infile = openFile(filename);
+        vector<double> numbers = readNumbers(infile);
 
+        // Calculate statistics
+        size_t count_number = numbers.size();
+        double sum_number = 0.0;
 
-    double number;
-    double sum_number = 0.0;
-    int count_number = 0;
+        for (double num : numbers)
+        {
+            sum_number += num;
+        }
 
-    infile >> number;              // Read the first number
-    while (infile.good())          // As long as the input is good and not EOF
-    {
-        ++ count_number;
-        sum_number += number;
-        infile >> number;
-    }
+        double average = count_number > 0 ? sum_number / count_number : 0.0;
 
-    if (infile.eof())
-        cout << "End of file." << endl;
-    else if (infile.fail())
-        cout << "Reading stopped, bad data." << endl;
-    else
-        cout << "Reading failed." << endl;
-
-    if (count_number == 0)
-        cout << "No data was read." << endl;
-
-    else
-    {
+        // Print statistics to the console
+        cout << "File: " << filename << endl;
         cout << "Total count of numbers: " << count_number << endl;
         cout << "Their sum is: " << sum_number << endl;
-        cout << "Their average is: " << sum_number/count_number << endl;
-        outfile.open("output.txt");
-        outfile << "Total count of numbers: " << count_number << endl;
-        outfile << "Their sum is: " << sum_number << endl;
-        outfile << "Their average is: " << sum_number/count_number << endl;
-        outfile.close();
-    }
+        cout << "Their average is: " << average << endl;
 
-    infile.close();
+        // Write statistics to output file (optional)
+        ofstream outfile("output.txt");
+        if (outfile.is_open())
+        {
+            outfile << "File: " << filename << endl;
+            outfile << "Total count of numbers: " << count_number << endl;
+            outfile << "Their sum is: " << sum_number << endl;
+            outfile << "Their average is: " << average << endl;
+            outfile.close();
+        } 
+        else 
+        {
+            cerr << "Warning: Could not open output file 'output.txt'." << endl;
+        }
+
+        infile.close();
+
+    } 
+    catch (const runtime_error& e)
+    {
+        cerr << "Error: " << e.what() << endl;
+    }
 
     return 0;
 }
